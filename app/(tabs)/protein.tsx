c8deaaacfed501598501cb3bc58425c48af31d49
bush <busra_hekim17@outlook.com>
2024-07-31 14:01:51 +0300

    useUserDoc custom hook

--- app/(tabs)/protein.tsx
+++ app/(tabs)/protein.tsx
@@ -1,41 +1,10 @@
 import { View, Text, ActivityIndicator, TouchableOpacity } from "react-native";
-import React, { useEffect, useState } from "react";
-import { getAuth } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
-import { FIRESTORE_DB } from "@/firebaseConfig";
+import React from "react";
 import { BlurView } from "expo-blur";
+import useUserDoc from "@/hooks/useUserDoc";
 
 const Protein = () => {
-  const [isPremium, setIsPremium] = useState<boolean | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const auth = getAuth();
-        const user = auth.currentUser;
-
-        if (user) {
-          const userDocRef = doc(FIRESTORE_DB, "users", user.uid);
-          const userDoc = await getDoc(userDocRef);
-
-          if (userDoc.exists()) {
-            setIsPremium(userDoc.data()?.isPremium || false);
-          } else {
-            console.log("No such document!");
-          }
-        } else {
-          console.log("No authenticated user found!");
-        }
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchUserData();
-  }, []);
+  const { userDoc, loading, error } = useUserDoc();
 
   if (loading) {
     return (
@@ -44,6 +13,16 @@ const Protein = () => {
       </View>
     );
   }
+
+  if (error) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <Text className="text-lg font-bold text-red-500">{error}</Text>
+      </View>
+    );
+  }
+
+  const isPremium = userDoc?.isPremium || false;
 
   return (
     <View className="flex-1 justify-center items-center p-4">
